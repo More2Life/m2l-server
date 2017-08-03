e7e18174f108a7daa636c64a1c9456f1f9dc096d
Brendan Kingsford <bkingsford@apple.com>
2017-08-03 14:42:02 -0700

    Listing controller

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -1,3 +1,20 @@
-/**
- * Created by bkingsford on 8/3/17.
- */
+var mongoose = require('mongoose');
+var Event = require('../models/listing').Listing;
+var request = require('request');
+const TOKEN = process.env.SQUARE_PERSONAL_ACCESS_TOKEN;
+var SQUARE_BASE_URL = 'https://connect.squareup.com/v2/';
+
+var options = {
+    headers: {
+        'Authorization': TOKEN,
+        'Accept': 'application/json'
+    }
+};
+
+var EventController = {
+    updateListing : function (req, callback) {
+
+    }
+}
+
+exports.EventController = EventController;
