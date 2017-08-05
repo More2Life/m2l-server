ab9f9ae3119375d63e16af6a960c49209befdab4
Todd Wilson <todd@dodgertodd.com>
2017-08-04 22:32:02 -0700

    Javascript is hard

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,18 +1,49 @@
 var mongoose = require('mongoose');
 var Event = require('../models/Event').Event;
 var request = require('request');
-var TOKEN = process.env.EVENTBRITE_BEARER_TOKEN;
+const TOKEN = process.env.EVENTBRITE_BEARER_TOKEN;
 var VENUEURI = "https://www.eventbriteapi.com/v3/venues/"
 
-var EventController = {
-    var eventbriteRequest = request.defaults({
-        headers: {
-            'Authorization' : TOKEN
-        },
-        json: true
-    });
+var eventbriteRequest = request.defaults({
+    headers: {
+        'Authorization' : TOKEN
+    },
+    json: true
+})
 
-    handleEventbriteHook : function (req, callback) {
+function createEvent(eventBody, callback) {
+    var uri = VENUEURI.concat(eventBody.venue_id);
+    console.log("Venue URI", uri);
+    eventbriteRequest(uri, function(error, response, body) {
+        console.log("Venue Error", error);
+        console.log("Venue Response", response);
+        console.log("Venue body", body);
+        var status = (eventBody.status === 'live') ? true : false;
+        var event = new Event({
+            title: eventBody.name.text,
+            description: eventBody.description.text,
+            index: 0,
+            isActive: status,
+            eventUrl: eventBody.url,
+            resourceUri: eventBody.resource_uri,
+            imageUrl: eventBody.logo.original.url,
+            startTime: eventBody.start.local,
+            endTime: eventBody.end.local,
+            address: body.address.localized_address_display,
+            area: body.address.localized_area_display,
+            multiLineAddress: body.address.localized_multi_line_address_display,
+            latitude: body.address.latitude,
+            longitude: body.address.longitude,
+            venueName: body.name
+        });
+        console.log("Event", event);
+        event.save(callback);
+    });
+}
+
+var EventController = {
+
+    handleEventbriteHook: function(req, callback) {
         var action = req.config.action;
         var url = req.api_url;
         console.log("Eventbrite action", action);
@@ -40,40 +71,7 @@ var EventController = {
         else {
             return;
         }
-    };
-
-    function createEvent(eventBody, callback) {
-        var uri = VENUEURI.concat(eventBody.venue_id);
-        console.log("Venue URI", uri);
-        eventbriteRequest(uri, function(error, response, body) {
-            console.log("Venue Error", error);
-            console.log("Venue Response", response);
-            console.log("Venue body", body);
-            var status = (eventBody.status === 'live') ? true : false;
-            var event = new Event({
-                title: eventBody.name.text,
-                description: eventBody.description.text,
-                index: 0,
-                isActive: status,
-                eventUrl: eventBody.url,
-                resourceUri: eventBody.resource_uri,
-                imageUrl: eventBody.logo.original.url,
-                startTime: eventBody.start.local,
-                endTime: eventBody.end.local,
-                address: body.address.localized_address_display,
-                area: body.address.localized_area_display,
-                multiLineAddress: body.address.localized_multi_line_address_display,
-                latitude: body.address.latitude,
-                longitude: body.address.longitude,
-                venueName: body.name
-            });
-            console.log("Event", event);
-            event.save(callback);
-        });
-    };
-
-
-
+    }
 }
 
 exports.EventController = EventController;
