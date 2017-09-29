de343d267ddcac1a1c5e4f53ea32a55bb2d89dde
Brendan Kingsford <bkingsford@apple.com>
2017-09-29 11:30:58 -0700

    auto increment index

--- models/feedItem.js
+++ models/feedItem.js
@@ -1,4 +1,9 @@
 var mongoose = require( 'mongoose' );
+var autoIncrement = require('mongoose-auto-increment');
+
+var connection = mongoose.createConnection(process.env.IS_PRODUCTION ? process.env.MONGODB_URI : process.env.MONGODB_URI_TEST);
+autoIncrement.initialize(connection);
+
 
 // create feedItem schema
 var options = { discriminatorKey: 'type' };
@@ -14,5 +19,10 @@ var feedItemSchema = new mongoose.Schema({
     isActive: Boolean
 }, options);
 
+
+// auto increment index
+feedItemSchema.plugin(autoIncrement.plugin, { model: 'FeedItem', field: 'index' });
+var FeedItem = mongoose.model('feeditems', feedItemSchema);
+
 // create model
-exports.FeedItem = mongoose.model('feeditems', feedItemSchema);
+exports.FeedItem = FeedItem;
