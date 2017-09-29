93a3ec225ffe6d0471a7ecfa6ec870ff6a64ec22
Brendan Kingsford <bkingsford@apple.com>
2017-09-29 14:39:47 -0700

    replace accidental delete

--- controllers/feedItemController.js
+++ controllers/feedItemController.js
@@ -6,11 +6,17 @@ var FeedItemController = {
         var requestedCount = parseInt(req.query.count);
         var requestedIndex = req.query.index;
         var requestedType = req.query.type;
+        var requestedIsActive = req.query.isActive;
         console.log('Requested Count: ' + requestedCount);
         console.log('Requested Index: ' + requestedIndex);
         console.log('Requested Type: ' + requestedType);
+        console.log('Requested isActive: ' + requestedIsActive);
 
         var query = FeedItem.find();
+
+        if (requestedIsActive) {
+            query = query.where('isActive').equals(requestedIsActive);
+         }
 
         if (requestedType) {
             query = query.where('type').equals(requestedType);
