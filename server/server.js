93963bd7f297c8908c6444ef26d7535038fdf32e
Brendan Kingsford <bkingsford@apple.com>
2017-10-25 17:51:11 -0700

    Verify shopify webhook hash

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var mongoose    = require('mongoose');
 var database    = require('./database/database');
 var createError = require('http-errors');
 var path        = require('path');
+var crypto      = require('crypto');
 
 // MODELS
 // =============================================================================
@@ -20,14 +21,38 @@ var Donation    = require('./models/donation').Donation;
 // CONFIGURE APP
 // =============================================================================
 var app         = express();
-var api         = require ('./routes/api');
-var webhooks    = require('./routes/webhooks');
+
+app.use((req, res, next) => {
+
+    req.rawBody = '';
+    req.on('data', function(chunk) {
+        req.rawBody += chunk;
+    });
+
+    req.on('end', function() {
+        // Validate shopify webhook token. If the hashes don't match, reject the request
+        if (req.url.search('.*\/shopify\/.*') >= 0) {
+            const SHOPIFY_SHARED_SECRET = process.env.SHOPIFY_WEBHOOK_SECRET;
+            var calculated_signature = crypto.createHmac('sha256', SHOPIFY_SHARED_SECRET)
+                .update(req.rawBody)
+                .digest('base64');
+            if (calculated_signature != req.headers['x-shopify-hmac-sha256']) {
+                res.status(403).json({error: "Access Denied"});
+                throw new Error('Invalid signature. Access denied');
+            }
+        }
+    });
+
+    next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // REGISTER OUR ROUTES
 // =============================================================================
+var api         = require ('./routes/api');
+var webhooks    = require('./routes/webhooks');
 app.use(express.static(path.join(__dirname, '../web/build')));  // Serve static files from the React app
 
 app.use('/api', api);
